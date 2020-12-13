cb5f8938ec93ca8e9fd248e32c34e4f438d869c4
ardipta <ashiqur35-2149@diu.edu.bd>
2020-12-13 23:10:19 +0600

    some css changed

--- semester_preregistration_portal/settings.py
+++ semester_preregistration_portal/settings.py
@@ -180,8 +180,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ardipta82@gmail.com'
-EMAIL_HOST_PASSWORD = 'google82@gmail.com'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
 
 
 from django.contrib.messages import constants as messages
